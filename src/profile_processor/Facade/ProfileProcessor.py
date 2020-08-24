65157702b0efb2bdc817d434a2e2c0423ffe655d
Sergio Barbosa <s.barbosa@chargebacks911.com>
2020-08-23 21:18:13 -0400

    [update] Pytest was set it up. [update] Started TDD approach.

---
+++ src/profile_processor/Facade/ProfileProcessor.py
@@ -0,0 +1,23 @@
+from cerberus import Validator
+from src.profile_processor.Schema.User import user
+
+
+class ProfileProcessor:
+
+    @staticmethod
+    def validate(user_schema: dict) -> bool:
+        v = Validator()
+        v.validate(user_schema, user())
+        return True
+
+    @staticmethod
+    def sanitize() -> bool:
+        return True
+
+    @staticmethod
+    def save() -> bool:
+        return True
+
+    @staticmethod
+    def process() -> bool:
+        return True
---
+++ tests/functional/ProfileProcessor/Facade/test_ProfileProcessor.py
@@ -0,0 +1,6 @@
+from src.profile_processor.Facade.ProfileProcessor import ProfileProcessor
+
+
+def test_was_factory_returning_errors():
+    wrongUserProfile = {'name': 'Juan', 'identification_number': 103058669}
+    assert ProfileProcessor.validate(wrongUserProfile)

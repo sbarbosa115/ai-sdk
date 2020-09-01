4a40c8b94d61ea68499de972b41e4398bc2dc650
JennLeonFonseca <jennfonseca@outlook.com>
2020-09-01 12:41:47 -0500

    ToDo de areas y sectores

---
+++ src/profile_processor/Model/ExperienceCategories/AreaLevel.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class AreaLevel(Enum):
+# TO DO definir que fuente de areas colocar
+    @staticmethod
+    def all() -> list:
+        return [
+        ]
---
+++ src/profile_processor/Model/ExperienceCategories/SectorLevel.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class SectorLevel(Enum):
+# TO DO definir que fuente de sectores colocar
+    @staticmethod
+    def all() -> list:
+        return [
+        ]

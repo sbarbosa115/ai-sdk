06e1556aaacc84368a5d5adfb13221431ab3b5fa
Sergio Barbosa <s.barbosa@chargebacks911.com>
2020-09-01 22:23:44 -0400

    [update] S3 load implementation completed.

--- src/profile_processor/Facade/ProfileProcessor.py
+++ src/profile_processor/Facade/ProfileProcessor.py
@@ -1,5 +1,14 @@
+import json
+import logging
+import os
+import uuid
+import boto3
 from cerberus import Validator
+from dotenv import load_dotenv, find_dotenv
 from src.profile_processor.Schema.User import user
+
+load_dotenv(find_dotenv())
+logging.basicConfig(filename='../../var/logs/transaction.log')
 
 
 class ProfileProcessor:
@@ -11,14 +20,30 @@ class ProfileProcessor:
         return v.errors
 
     @staticmethod
-    def save() -> dict:
+    def save(user_schema: dict) -> bool:
+        AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
+        AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
+        AWS_BUCKET_NAME = os.environ.get("AWS_BUCKET_NAME")
+
+        session = boto3.Session(
+            aws_access_key_id=AWS_ACCESS_KEY,
+            aws_secret_access_key=AWS_SECRET_KEY,
+        )
+
+        s3 = session.resource('s3')
+        s3object = s3.Object(AWS_BUCKET_NAME, '{}.{}'.format(str(uuid.uuid4()), 'json'))
+        s3object.put(Body=json.dumps(user_schema).encode('UTF-8'))
+
         return True
 
     @staticmethod
     def process(user_schema: dict) -> dict:
+        logging.log('Loading User {} '.format(json.dumps(user_schema).encode('UTF-8')))
+
         v = Validator()
         v.validate(user_schema, user())
         if len(v.errors) == 0:
+            ProfileProcessor.save(user_schema)
             return v.document
 
         return v.errors
--- tests/functional/ProfileProcessor/Facade/test_Studies.py
+++ tests/functional/ProfileProcessor/Facade/test_Studies.py
@@ -2,7 +2,7 @@ from src.profile_processor.Facade.ProfileProcessor import ProfileProcessor
 from tests.Factory import UserProfile
 
 
-def test_when_date_is_present_must_be_grater_than_from_date():
+def test_when_to_date_is_present_must_be_grater_than_from_date():
     profile = UserProfile.profile()
 
     start_date = '2020-01-03'
@@ -11,4 +11,4 @@ def test_when_date_is_present_must_be_grater_than_from_date():
     profile['studies'][0]['end_date'] = end_date
     errors = ProfileProcessor.validate(profile)
 
-    assert len(errors) > 0
+    assert len(errors) == 0

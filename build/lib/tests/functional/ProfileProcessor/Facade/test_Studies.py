9073d26fa5db62cb7f1a1357bf22eead7469ca51
Sergio Barbosa <s.barbosa@chargebacks911.com>
2020-09-01 22:41:43 -0400

    [update] SYNC Repo

---
+++ build/lib/tests/functional/ProfileProcessor/Facade/test_Studies.py
@@ -0,0 +1,14 @@
+from src.profile_processor.Facade.ProfileProcessor import ProfileProcessor
+from tests.Factory import UserProfile
+
+
+def test_when_to_date_is_present_must_be_grater_than_from_date():
+    profile = UserProfile.profile()
+
+    start_date = '2020-01-03'
+    end_date = '2020-01-02'
+    profile['studies'][0]['start_date'] = start_date
+    profile['studies'][0]['end_date'] = end_date
+    errors = ProfileProcessor.validate(profile)
+
+    assert len(errors) == 0

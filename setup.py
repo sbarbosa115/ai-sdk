aaff7d388154e45c55d14b2cced332eee4dacf33
Sergio Barbosa <s.barbosa@chargebacks911.com>
2020-09-04 16:55:43 -0400

    [update] Working in [Date range validations].

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import setuptools
 
-
 setuptools.setup(
     name='ai-sdk-sbarbosa115',
     version='1.0.3',
@@ -10,6 +9,7 @@ setuptools.setup(
     author='Sergio Barbosa ',
     author_email='sbarbosa115@gmail.com',
     description='IA package.',
+    package_dir={'ia_sdk': 'src'},
     install_requires=[
         'cerberus', 'pycountry', 'boto3', 'python-dotenv'
     ],
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from src import profile_processor as profile_processor
